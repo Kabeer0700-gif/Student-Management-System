b49c50619ee6b9756013b1f26d0caf9539b82052
Kabeer0700-gif <kb3463217@gmail.com>
2026-06-21 15:07:32 +0500

    add student class constructor

---
+++ student.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self,student_id,name,age,cgpa,department,email):
+        self.student_id = student_id
+        self.name = name
+        self.age = age
+        self.cgpa = cgpa
+        self.department = department
+        self.email = email
+
+        
