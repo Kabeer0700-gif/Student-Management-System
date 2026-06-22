003694b92d7c4e3a738798581141273664e4c6ee
Kabeer0700-gif <kb3463217@gmail.com>
2026-06-22 12:28:10 +0500

    remove count variable and add search and delete methods

--- manager.py
+++ manager.py
@@ -1,10 +1,7 @@
 class Manager:
     students = []
-    count = 0
-
     @classmethod
     def add_student(cls,std):
-        cls.count += 1
         cls.students.append(std)
         print(
             f"added student with student id: {std.student_id}"
@@ -14,7 +11,7 @@ class Manager:
     @classmethod
     def view_student(cls):
         print("------------- STUDENT INFORMATION ------------------")
-        for i in range(cls.count):
+        for i,student in enumerate(cls.students):
             print(
                 f"Sno: {i+1}\n"
                 f"ID: {cls.students[i].student_id}\n"
@@ -27,6 +24,35 @@ class Manager:
 
             print("------------------------------------------------")
 
+
+    @classmethod
+    def search_student(cls,id):
+        for i,student in enumerate(cls.students):
+            if cls.students[i].student_id == id:
+                print(
+                    f"ID: {cls.students[i].student_id}\n"
+                    f"Name: {cls.students[i].name}\n"
+                    f"Age: {cls.students[i].age}\n"
+                    f"Department: {cls.students[i].department}\n"
+                    f"CGPA: {cls.students[i].cgpa}\n"
+                    f"Email: {cls.students[i].email}\n"
+                )
+                print("")
+
+                break
+
+    
+    @classmethod
+    def delete_student(cls,id):
+        for i,student in enumerate(cls.students):
+            if cls.students[i].student_id == id:
+                del cls.students[i]
+                return True
+        
+        return False
+    
+
+                
 
 
 
