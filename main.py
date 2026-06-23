b5e968718bd280b22b1499f54eff5c8983235f69
Kabeer0700-gif <kb3463217@gmail.com>
2026-06-24 00:38:54 +0500

    added view and add student in CLI

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from storage import Storage
 from student import Student
 
 
+manager = Manager()
+
 while True:
-    print("================== STUDENT MANAGEMENT SYSTEM =====================")
+    print("\n================== STUDENT MANAGEMENT SYSTEM =====================")
     print("1. Add Student")
     print("2. View Student")
     print("3. Search Student")
@@ -17,4 +19,20 @@ while True:
     print("9. Save Data")
     print("0. Exit")
 
-    
+
+    choice = input("Enter your choice:\t")
+    match choice:
+        case '1':
+            id = int(input("Enter id: "))
+            name = input("Enter Name: ")
+            age = input("Enter Age: ")
+            department = input("Enter Department: ")
+            cgpa = input("Enter CGPA: ")
+            email = input("Enter email: ")
+            student = Student(id,name,age,cgpa,department,email)
+            manager.add_student(student)
+
+            print("\n---------------Student Added Successfully-------------------")
+
+        case '2':
+            manager.view_student()
